c4c6a13675ee6ca08de65807afbd85610b2110cb
Daivd Perkind <joeperkins@outlook.com>
2022-10-02 20:39:57 +0100

    rename views - missed file

--- COMMAND/build_rename_views.sql
+++ COMMAND/build_rename_views.sql
@@ -18,25 +18,23 @@ GO
 DROP VIEW IF EXISTS COLLECTION.base
 GO
 
-DROP VIEW IF EXISTS COLLECTION.base_INK_COLLECTION
-GO
-
-DROP VIEW IF EXISTS COLLECTION.base_OS_EXPLORER
-GO
+-- FUNCTIONS
+:r "COLLECTION/Functions/COLLECTION.child_column_list.sql"
 
 -- VIEWS
 :r "COLLECTION/Views/COLLECTION.v_attribute_list.sql"
 :r "COLLECTION/Views/COLLECTION.v_base.sql"
-:r "COLLECTION/Views/COLLECTION.v_base_INK_COLLECTION.sql"
-:r "COLLECTION/Views/COLLECTION.v_base_OS_EXPLORER.sql"
 
 -- STORED PROCEDURES
 :r "CORE/StoredProcedures/CORE.cu_collection_item.sql"
 :r "CORE/StoredProcedures/CORE.d_collection_item.sql"
 :r "CORE/StoredProcedures/CORE.ru_attribute.sql"
 :r "COLLECTION/StoredProcedures/COLLECTION.r_collection_crystallise.sql"
-
 GO
+
+EXEC COLLECTION.r_collection_crystallise 'INK COLLECTION'
+EXEC COLLECTION.r_collection_crystallise 'OS EXPLORER'
+EXEC COLLECTION.r_collection_crystallise 'OS LANDRANGER'
 
 PRINT 'SCRIPT COMPLETE'
 GO

cf096614730c661bc8cced10f8685998fbe9fd8a
David Perkins <joeperkins@outlook.com>
2022-10-02 11:58:36 +0100

    standardise - tidy up filename and command file

--- COMMAND/build_standardise_object_drop_create.sql
+++ COMMAND/build_standardise_object_drop_create.sql
@@ -15,13 +15,17 @@ SET NOCOUNT ON
 DROP PROCEDURE IF EXISTS COLLECTION.cu_collection_item_json
 GO
 
+-- FUNCTIONS
 :r "COLLECTION/Functions/COLLECTION.child_column_list.sql"
 :r "COLLECTION/Functions/COLLECTION.collection_id.sql"
 :r "COLLECTION/Functions/COLLECTION.column_list_convert.sql"
+
 :r "CORE/Functions/CORE.script_collection_sql.sql"
+
 :r "UTILS/Functions/UTILS.sb.sql"
 :r "UTILS/Functions/UTILS.sq.sql"
 
+-- STORED PROCEDURES
 :r "COLLECTION/StoredProcedures/COLLECTION.c_collection_item_json.sql"
 :r "COLLECTION/StoredProcedures/COLLECTION.c_item_attribute_field_json.sql"
 :r "COLLECTION/StoredProcedures/COLLECTION.c_item_attribute_field_json.sql"
@@ -32,7 +36,8 @@ GO
 :r "CORE/StoredProcedures/CORE.cu_collection_item.sql"
 :r "CORE/StoredProcedures/CORE.d_collection_item.sql"
 :r "CORE/StoredProcedures/CORE.ru_attribute.sql"
-:r "UTILS/StoredProcedures/r_table_columns.sql"
+
+:r "UTILS/StoredProcedures/UTILS.r_table_columns.sql"
 GO
 
 PRINT 'SCRIPT COMPLETE'

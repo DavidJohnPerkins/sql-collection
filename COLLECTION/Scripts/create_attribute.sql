ebb30425cec3496535d775feadcf173f484bfcd4
DavidJohnPerkins <DavidJohnPerkins@users.noreply.github.com>
2025-01-06 23:25:01 +0000

    New scripts

--- COLLECTION/Scripts/create_attribute.sql
+++ COLLECTION/Scripts/create_attribute.sql
@@ -1,11 +1,7 @@
 USE Collections
 GO
 
-INSERT INTO COLLECTION.item_attribute_field (item_attr_name, item_attr_data_type)
-VALUES	('NAME', 'V') ,
-		('DESCRIPTION', 'V') ,
-		('STARTED', 'D'),
-		('MAP NUMBER', 'I'),
-		('MAP TITLE', 'V'),
-		('PUBLISH DATE', 'D'),
-		('MAIN SETTLEMENTS', 'V')
+INSERT INTO COLLECTION.item_attribute_field (item_attr_name, type_id)
+VALUES	('RGB_RED', 4) ,
+		('RGB_GREEN', 4) ,
+		('RGB_BLUE', 4)

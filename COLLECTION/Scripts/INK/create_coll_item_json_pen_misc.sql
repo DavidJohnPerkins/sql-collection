85a42e2b8b90fa82ee2c113bf1ee9c1c70bb4af4
DavidJohnPerkins <DavidJohnPerkins@users.noreply.github.com>
2025-10-06 00:08:18 +0100

    Add two pens

--- COLLECTION/Scripts/INK/create_coll_item_json_pen_misc.sql
+++ COLLECTION/Scripts/INK/create_coll_item_json_pen_misc.sql
@@ -8,33 +8,34 @@ SET @json_data = N'
 	"parent_collection": "PEN COLLECTION",
 	"item":[
 		{
-			"item_key_value": "PILOT001",
+			"item_key_value": "FAB001",
 			"item_attribute": [
-				{"attr_name": "KEY_VALUE",			"attr_value": "PILOT001"},
+				{"attr_name": "KEY_VALUE",			"attr_value": "FAB001"},
 				{"attr_name": "WRITING_TYPE",		"attr_value": "Fountain Pen"},
-				{"attr_name": "BRAND",				"attr_value": "Pilot"},
-				{"attr_name": "RANGE",				"attr_value": "Capless"},
-				{"attr_name": "MODEL_NAME",			"attr_value": "Classic"},
-				{"attr_name": "BODY_MATERIAL",		"attr_value": "Brass"},
-				{"attr_name": "BODY_COLOUR", 		"attr_value": "Matt Black"},
-				{"attr_name": "GRIP_MATERIAL",		"attr_value": "Brass"},
-				{"attr_name": "GRIP_COLOUR",		"attr_value": "Matt Black"},
-				{"attr_name": "CAP_MATERIAL",		"attr_value": "N/A"},
-				{"attr_name": "CAP_COLOUR",			"attr_value": "N/A"},
-				{"attr_name": "NIB_MATERIAL",		"attr_value": "Gold"},
+				{"attr_name": "BRAND",				"attr_value": "Faber-Castell"},
+				{"attr_name": "RANGE",				"attr_value": "Grip"},
+				{"attr_name": "MODEL_NAME",			"attr_value": "Grip Fountain Pen"},
+				{"attr_name": "BODY_MATERIAL",		"attr_value": "Plastic"},
+				{"attr_name": "BODY_COLOUR", 		"attr_value": "Blue / Light Blue Dots"},
+				{"attr_name": "GRIP_MATERIAL",		"attr_value": "Plastic"},
+				{"attr_name": "GRIP_COLOUR",		"attr_value": "Matt Grey"},
+				{"attr_name": "CAP_MATERIAL",		"attr_value": "Plastic"},
+				{"attr_name": "CAP_COLOUR",			"attr_value": "Blue"},
+				{"attr_name": "NIB_MATERIAL",		"attr_value": "Steel"},
 				{"attr_name": "NIB_COLOUR",			"attr_value": "Chrome"},
-				{"attr_name": "NIB_SIZE", 			"attr_value": "Fine"},
-				{"attr_name": "PURCHASE_PRICE", 	"attr_value": 220},
-				{"attr_name": "REPLACEMENT_PRICE",	"attr_value": 230},
-				{"attr_name": "RATING", 			"attr_value": 5}
+				{"attr_name": "NIB_SIZE", 			"attr_value": "Medium"},
+				{"attr_name": "PURCHASE_PRICE", 	"attr_value": 15},
+				{"attr_name": "REPLACEMENT_PRICE",	"attr_value": 15},
+				{"attr_name": "RATING", 			"attr_value": 4}
 			],
 			"item_comment": [
-				{"comment":     "Purchased in Seville"}
+                {"comment":     "Purchased in Pordenone"}
 			]
 		}
 	]
 }]
 ';
+
 
 --select @json_data;
 EXEC COLLECTION.c_collection_item @json_data, 0, 1

601a818d94b1bfe9d9990af08b8c88004bdc00df
DavidJohnPerkins <DavidJohnPerkins@users.noreply.github.com>
2026-05-03 20:19:54 +0100

    Running in Docker

--- COLLECTION/Scripts/INK/create_item_json_ink_taccia.sql
+++ COLLECTION/Scripts/INK/create_item_json_ink_taccia.sql
@@ -8,31 +8,31 @@ SET @json_data = N'
     "parent_collection": "INK COLLECTION",
     "item":[
 		{
-			"item_key_value": "TA002",
+			"item_key_value": "TA004",
 			"item_attribute": [
-					{"attr_name": "KEY_VALUE",		"attr_value": "TA002"},
+					{"attr_name": "KEY_VALUE",		"attr_value": "TA004"},
 					{"attr_name": "BRAND",			"attr_value": "Taccia"},
 					{"attr_name": "RANGE",			"attr_value": "Sunaoiro"},
-					{"attr_name": "INK_NAME", 		"attr_value": "Ao"},
+					{"attr_name": "INK_NAME", 		"attr_value": "Midori"},
 					{"attr_name": "CONTAINER", 		"attr_value": "Bottle"},
 					{"attr_name": "CONTAINER_SIZE", "attr_value": "40ml"},
-					{"attr_name": "COLOUR", 		"attr_value": "Blue"},
+					{"attr_name": "COLOUR", 		"attr_value": "Green"},
 					{"attr_name": "QTY_REMAINING", 	"attr_value": 40},
 					{"attr_name": "RATING", 		"attr_value": 5}
 			]
 		},
 		{
-			"item_key_value": "TA003",
+			"item_key_value": "TA005",
 			"item_attribute": [
-					{"attr_name": "KEY_VALUE",		"attr_value": "TA003"},
+					{"attr_name": "KEY_VALUE",		"attr_value": "TA005"},
 					{"attr_name": "BRAND",			"attr_value": "Taccia"},
-					{"attr_name": "RANGE",			"attr_value": "Jeans"},
-					{"attr_name": "INK_NAME", 		"attr_value": "Dark Navy"},
+					{"attr_name": "RANGE",			"attr_value": "Ukiyoe"},
+					{"attr_name": "INK_NAME", 		"attr_value": "koiai"},
 					{"attr_name": "CONTAINER", 		"attr_value": "Bottle"},
 					{"attr_name": "CONTAINER_SIZE", "attr_value": "40ml"},
 					{"attr_name": "COLOUR", 		"attr_value": "Blue"},
 					{"attr_name": "QTY_REMAINING", 	"attr_value": 40},
-					{"attr_name": "RATING", 		"attr_value": 4}
+					{"attr_name": "RATING", 		"attr_value": 5}
 			]
 		}
 	]

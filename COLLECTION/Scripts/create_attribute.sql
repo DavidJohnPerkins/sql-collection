ab1486a98eec23ea0ef87e3552c0b6b05a7a36be
DavidJohnPerkins <DavidJohnPerkins@users.noreply.github.com>
2026-05-28 09:49:30 +0100

    Polychromos finished

--- COLLECTION/Scripts/create_attribute.sql
+++ COLLECTION/Scripts/create_attribute.sql
@@ -2,10 +2,4 @@ USE Collections
 GO
 
 INSERT INTO COLLECTION.item_attribute_field (item_attr_name, type_id, return_length) VALUES
-('PUBLISHER', 1, 255),
-('COMPOSER', 1, 255),
-('WORK_NAME', 1, 255),
-('WORK_TYPE', 1, 50),
-('SCORE_SIZE', 1, 50)
-INSERT INTO COLLECTION.item_attribute_field (item_attr_name, type_id) VALUES
-('SCORE_PAGES', 4)
+('URL_FRAGMENT', 1, 255)
